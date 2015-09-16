ec64600b4897a87d66de9dcb39cb6b9d1d8a1a6b
Malotte Westman Lönne <malotte@malotte.net>
2015-09-16 14:43:31 +0200

    Made nmea_2000 into a server sending data to hexs subscribers.

--- src/nmea_2000.erl
+++ src/nmea_2000.erl
@@ -1,19 +1,75 @@
+%%%---- BEGIN COPYRIGHT -------------------------------------------------------
+%%%
+%%% Copyright (C) 2015, Rogvall Invest AB, <tony@rogvall.se>
+%%%
+%%% This software is licensed as described in the file COPYRIGHT, which
+%%% you should have received as part of this distribution. The terms
+%%% are also available at http://www.rogvall.se/docs/copyright.txt.
+%%%
+%%% You may opt to use, copy, modify, merge, publish, distribute and/or sell
+%%% copies of the Software, and permit persons to whom the Software is
+%%% furnished to do so, under the terms of the COPYRIGHT file.
+%%%
+%%% This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+%%% KIND, either express or implied.
+%%%
+%%%---- END COPYRIGHT ---------------------------------------------------------
 %%% @author Tony Rogvall <tony@rogvall.se>
 %%% @copyright (C) 2015, Tony Rogvall
 %%% @doc
+%%% NMEA 2000 application api.
 %%%
+%%% File: nmea_200.erl <br/>
+%%% Created:  9 Sep 2015 by Tony Rogvall
 %%% @end
-%%% Created :  9 Sep 2015 by Tony Rogvall <tony@rogvall.se>
-
+%%%-------------------------------------------------------------------
 -module(nmea_2000).
 
 -include_lib("can/include/can.hrl").
 -include("../include/nmea_2000.hrl").
 
+%% Function API
 -export([file/1]).
+-export([join/1]).
+-export([input/1]).
+
+%% Test api
+-export([test/1]).
+
+-define(SERVER, nmea_2000_srv).
 
 -define(dbg(F,A), ok).
 %% -define(dbg(F,A), io:format((F),(A)).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Open a connection to the nmea server.
+%% @end
+%%--------------------------------------------------------------------
+-spec join(Params::list(term())) -> 
+			{ok, Id::term()} | 
+			{error, Error::term()}.
+
+join(Params) ->
+    gen_server:call(?SERVER, {join, self(), Params}).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Send data as a can_frame to the nmea server.
+%% @end
+%%--------------------------------------------------------------------
+-spec input(Frame::#can_frame{}) -> ok.
+
+input(Frame) when is_record(Frame, can_frame) ->
+    gen_server:cast(?SERVER, {input, self(), Frame}).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Process nmea data from a file.
+%% @end
+%%--------------------------------------------------------------------
+-spec file(Frame::string()) -> ok | 
+			       {error, Error::term()}.
 
 file(File) ->
     {ok,Out} = file:open("nmea.log", [write]),
@@ -75,4 +131,37 @@ loop(Fd, Fun, Dict) ->
 	CanFrame = #can_frame {} ->
 	    Dict1 = nmea_2000_packet:collect_packet(CanFrame, Fun, Dict),
 	    loop(Fd, Fun, Dict1)
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Process nmea data from a file and send to nmea_2000_srv
+%% @end
+%%--------------------------------------------------------------------
+-spec test(Frame::string()) -> ok | 
+			       {error, Error::term()}.
+
+test(File) ->
+    case nmea_2000_log:open(File) of
+	{ok, Fd} ->
+	    try test_loop(Fd) of
+		ok -> ok
+	    catch
+		error:Reason ->
+		    io:format("crash: ~p\n", 
+			      [erlang:get_stacktrace()]),
+		    {error, Reason}
+	    after
+		nmea_2000_log:close(Fd)
+	    end;
+	Error ->
+	    Error
+    end.
+
+test_loop(Fd) ->
+    case nmea_2000_log:read_can_frame(Fd) of
+	eof -> ok;
+	CanFrame = #can_frame {} ->
+	    nmea_2000:input(CanFrame),
+	    test_loop(Fd)
     end.
--- src/nmea_2000_sup.erl
+++ src/nmea_2000_sup.erl
@@ -1,9 +1,36 @@
+%%%---- BEGIN COPYRIGHT -------------------------------------------------------
+%%%
+%%% Copyright (C) 2015, Rogvall Invest AB, <tony@rogvall.se>
+%%%
+%%% This software is licensed as described in the file COPYRIGHT, which
+%%% you should have received as part of this distribution. The terms
+%%% are also available at http://www.rogvall.se/docs/copyright.txt.
+%%%
+%%% You may opt to use, copy, modify, merge, publish, distribute and/or sell
+%%% copies of the Software, and permit persons to whom the Software is
+%%% furnished to do so, under the terms of the COPYRIGHT file.
+%%%
+%%% This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+%%% KIND, either express or implied.
+%%%
+%%%---- END COPYRIGHT ---------------------------------------------------------
+%%% @author Tony Rogvall <tony@rogvall.se>
+%%% @author Marina Westman Lonne <malotte@malotte.net>
+%%% @copyright (C) 2015, Tony Rogvall
+%%% @doc
+%%%    Supervisor for nmea_2000 application.
+%%%
+%%% File: nmea_2000_sup.erl <br/>
+%%% Created:  September 2015 by Tony Rogvall
+%%% @end
+%%%-------------------------------------------------------------------
 -module(nmea_2000_sup).
 
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/1,
+	 stop/0]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -14,14 +41,53 @@
 %% ===================================================================
 %% API functions
 %% ===================================================================
+-spec start_link(Args::list({Key::atom(), Value::term()})) -> 
+			{ok, Pid::pid(), {normal, list()}} | 
+			{error, Error::term()}.
 
-start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+start_link(Args) ->
+    lager:info("args = ~p.\n", [Args]),
+    try supervisor:start_link({local, ?MODULE}, ?MODULE, Args) of
+	{ok, Pid} ->
+	    {ok, Pid, {normal, Args}};
+	Error -> 
+	    lager:error("Failed to start process, reason ~p.\n",  [Error]),
+	    Error
+    catch 
+	error:Reason ->
+	    lager:error("Try failed, reason ~p.\n", [Reason]),
+	    Reason
+
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Stops the server.
+%% @end
+%%--------------------------------------------------------------------
+-spec stop() -> ok.
+
+stop() ->
+    exit(normal).
 
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
-init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
-
+%%--------------------------------------------------------------------
+%% @private
+% @doc
+%% Starts the nmea_2000 server.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec init(Args::list({Key::atom(), Value::term()})) -> 
+		  {ok, {SupFlags::list(), ChildSpecs::list()}} |
+		  ignore |
+		  {error, Reason::term()}.
+init(Args) ->
+    lager:info("args = ~p,\n pid = ~p\n", [Args, self()]),
+    NS = nmea_2000_srv,
+    NSSpec = {NS, {NS, start_link, [Args]}, permanent, 5000, worker, [NS]},
+    lager:info("About to start ~p\n", [NSSpec]),
+    {ok, { {rest_for_one, 0, 300}, [NSSpec]} }.
